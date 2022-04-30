f12696601c62c234baf0b79daf9a625d6ea37a45
Phillip Eckstein <sirtario9@gmail.com>
2022-04-30 14:49:43 +0200

    Add simple login view

---
+++ src/main/java/Kiss/controller/LoginController.java
@@ -0,0 +1,12 @@
+package Kiss.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+    @FXML
+    private TextField UserNameInput;
+    @FXML
+    private PasswordField PasswordInput;
+}
