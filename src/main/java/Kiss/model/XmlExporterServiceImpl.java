11ea7bf73319952e5f0922548be2a38f6fc92381
Maximilian <maximilian.fischer.kh7@fh-zwickau.de>
2022-05-12 08:01:25 +0200

    Schreiben der Testklasse

--- src/main/java/Kiss/model/XmlExporterServiceImpl.java
+++ src/main/java/Kiss/model/XmlExporterServiceImpl.java
@@ -1,5 +1,6 @@
 package Kiss.model;
 
+import Kiss.dto.dtoAbteilung;
 import jakarta.xml.bind.JAXBContext;
 import jakarta.xml.bind.JAXBException;
 import jakarta.xml.bind.Marshaller;
@@ -16,50 +17,24 @@ public class XmlExporterServiceImpl implements XmlExporterService{
 
 
     @Override
-    public <T> void  xmlFileFromObject(T dto) {
-        Document doc = createDomFile(dto);
-    }
-
-    private <T> Document createDomFile (T dto) {
+    public <T> void xmlFileFromObject (T dto) {
 
         JAXBContext jaxbContext = null;
 
         try {
-            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-            dbf.setNamespaceAware(true);
-            DocumentBuilder db = dbf.newDocumentBuilder();
-            Document doc = db.newDocument();
-
-            // Normal JAXB RI
-            //jaxbContext = JAXBContext.newInstance(Fruit.class);
-
-            // EclipseLink MOXy needs jaxb.properties at the same package with Fruit.class
-            // Alternative, I prefer define this via eclipse JAXBContextFactory manually.
             jaxbContext = org.eclipse.persistence.jaxb.JAXBContextFactory
-                    .createContext(new Class[]{dto.getClass()}, null);
+                    .createContext(new Class[]{dtoAbteilung.class}, null);
 
             Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
-
-            // output pretty printed
             jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
 
-            // output to a xml file
-            jaxbMarshaller.marshal(dto, doc);
-
-            // output to console
-            // jaxbMarshaller.marshal(o, System.out);
-
-            return doc;
+            jaxbMarshaller.marshal(dto, new File("/ausgabe.xml"));
 
         } catch (JAXBException jaxbExc) {
             jaxbExc.printStackTrace();
-        } catch (ParserConfigurationException parExc) {
-
         } catch ( Exception e) {
 
         }
-
-        return null;
     }
 
 
--- src/test/java/Kiss/model/XmlExporterServiceTest.java
+++ src/test/java/Kiss/model/XmlExporterServiceTest.java
@@ -3,21 +3,37 @@ package Kiss.model;
 import Kiss.dto.dtoAbteilung;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class XmlExporterServiceTest {
 
-    XmlExporterService underTest;
+    XmlExporterService underTest = new XmlExporterServiceImpl();
 
     @BeforeEach
     public void setup() {
-        underTest = new XmlExporterServiceImpl();
+
     }
 
 
     @Test
    public void xmlFileFromObjectTest() {
-    underTest.xmlFileFromObject(createAbteilung());
 
+        String expect = "";
+        String actual = new String();
+        underTest.xmlFileFromObject(createAbteilung());
+
+        try{
+            Path fileName = Path.of("/ausgabe.xml");
+            actual = Files.readString(fileName);
+        } catch (IOException ioe) {
+            ioe.getMessage();
+        }
+
+        assertEquals(expect, actual);
 
    }
 
