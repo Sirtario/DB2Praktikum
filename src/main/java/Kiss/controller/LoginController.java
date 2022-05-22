9367994fa8f8805c0dd7badd7effd42f1f6835cb
Phillip Eckstein <sirtario9@gmail.com>
2022-05-22 14:36:24 +0200

    Add missing code to switch to main view after login

--- src/main/java/Kiss/controller/LoginController.java
+++ src/main/java/Kiss/controller/LoginController.java
@@ -2,13 +2,22 @@ package Kiss.controller;
 
 import Kiss.model.DatabaseConnector;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 import java.io.Console;
+import java.io.IOException;
 import java.sql.*;
 
 public class LoginController {
+
+    @FXML
+    private AnchorPane ViewBase;
     @FXML
     private TextField UserNameInput;
     @FXML
@@ -25,8 +34,23 @@ public class LoginController {
             return;
         }
         //create database connection with the credentials
-        ConnectToDB(UserNameInput.getText(), PasswordInput.getText());
+        if(ConnectToDB(UserNameInput.getText(), PasswordInput.getText()))
+        {
+            try {
+                LoadMainView();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 
+    private void LoadMainView() throws IOException {
+        Stage stage = (Stage) ViewBase.getScene().getWindow();
+
+        Parent root = FXMLLoader.load(getClass().getResource("../../view/main.fxml"));
+        stage.setTitle("KIS");
+        stage.setScene(new Scene(root, 400, 300));
+        stage.show();
     }
 
     private boolean CredentialsAreFilledIn() {
@@ -40,12 +64,14 @@ public class LoginController {
         return true;
     }
 
-    private void ConnectToDB(String user, String pass) {
+    private Boolean ConnectToDB(String user, String pass) {
 
         try{
             DatabaseConnector.ConnectToDB(UserNameInput.getText(), PasswordInput.getText());
+            return true;
         } catch (SQLException e) {
             e.printStackTrace();
+            return false;
         }
     }
 }
