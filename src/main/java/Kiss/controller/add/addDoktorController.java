817a4415582525816aafa3ca51727a914280fda2
Dominic Fitter <dominic.fitter.kgw@fh-zwickau.de>
2022-05-24 14:26:58 +0200

    add functionality to addBettController, addDiagnoseController, addDoktorController

--- src/main/java/Kiss/controller/add/addDoktorController.java
+++ src/main/java/Kiss/controller/add/addDoktorController.java
@@ -1,11 +1,55 @@
 package Kiss.controller.add;
 
 import Kiss.controller.MainViewController;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+import java.sql.SQLException;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 
 public class addDoktorController {
     private MainViewController mainViewController;
 
-    public addDoktorController(MainViewController mainViewController){
+    @FXML
+    private ComboBox<String> GeschlechtBox, KontaktdatenBox;
+    @FXML
+    private TextField NachnameText, VornameText, FachrichtungText;
+    @FXML
+    private DatePicker GeburtstagPicker;
+
+    public addDoktorController(MainViewController mainViewController) throws SQLException {
         this.mainViewController =  mainViewController;
+        populateGeschlechtBox();
+        populateKontaktdatenBox();
+        GeburtstagPicker = new DatePicker();
+    }
+
+    public void populateKontaktdatenBox() throws SQLException {
+        ArrayList<String> Kontaktdaten = mainViewController.getDb().returnForeigKeys("SELECT RaumID FROM Raum;");
+        for(String s : Kontaktdaten){
+            KontaktdatenBox.getItems().add(s);
+        }
+    }
+
+    public void populateGeschlechtBox() throws SQLException {
+        GeschlechtBox.getItems().addAll("w","m");
+    }
+
+    @FXML
+    private void onClickSaveEntry() throws SQLException {
+        String querry = "INSERT INTO Doktor (Vorname, Nachname, Geschlecht, Geburtstag, Fachrichtung, KontaktdatenID)" +
+                " VALUES ('"+VornameText+"', '"+NachnameText+"', '"+GeschlechtBox.getSelectionModel().getSelectedItem()+
+                "', '" +GeburtstagPicker.getValue().format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))+
+                "', '" +FachrichtungText+", "+KontaktdatenBox.getSelectionModel().getSelectedItem()+");";
+        mainViewController.getDb().runQuerry(querry);
+        mainViewController.getPopupStage().close();
+    }
+
+    @FXML
+    private void onClickCancel(){
+        mainViewController.getPopupStage().close();
     }
 }
